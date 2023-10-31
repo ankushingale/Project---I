ac5693b6672e7bdcc3c7f42c45b9a5d43a72bb83
Ankush Ingale <ankushingale958@gmail.com>
2023-10-31 16:16:55 +0530

    projectsetup

---
+++ ErgoAsia_project/customer_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def customerhome(request):
+    return render(request,'customer home.html')
---
+++ ErgoAsia_project/manufacturer_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def manufacturerhome(request):
+    return render(request,'manufacturer_home.html')
