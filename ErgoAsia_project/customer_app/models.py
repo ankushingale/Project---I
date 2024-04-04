b969ed38ea68467ee6f5ce7aa8a074ac9cc0db7f
Ankush Ingale <ankushingale958@gmail.com>
2024-04-04 16:24:36 +0530

    update

--- ErgoAsia_project/customer_app/models.py
+++ ErgoAsia_project/customer_app/models.py
@@ -7,6 +7,7 @@ class Customerdata(models.Model):
     phno = models.CharField(max_length=25)
     password = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
+    
 
     def __str__(self):
         return self.name
@@ -17,6 +18,14 @@ class Customerrequirements(models.Model):
     Part_Name = models.CharField(max_length=50)
     blank_name = models.CharField(max_length=50)
     draft = models.CharField(max_length=60)
-
+    cname=models.CharField(max_length=50)
+    pname=models.CharField(max_length=50)
+    cpno=models.CharField(max_length=50)
+    desc=models.CharField(max_length=50)
+    pr=models.CharField(max_length=50)
+    av=models.IntegerField(default=123)
+    qs=models.DateField()
+    tv=models.IntegerField()
+    sop=models.DateField()
     def __str__(self):
         return f"{self.customer.name} - {self.phone_no}"
