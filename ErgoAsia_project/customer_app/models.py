442b95d3c9989c10bdf660f6001ccac01ea45086
Ankush Ingale <ankushingale958@gmail.com>
2024-04-05 14:11:27 +0530

    new

--- ErgoAsia_project/customer_app/models.py
+++ ErgoAsia_project/customer_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from djongo import models as djongo_models
 
 class Customerdata(models.Model):
     customer_id = models.IntegerField(primary_key=True, unique=True, default=123)
@@ -11,13 +12,18 @@ class Customerdata(models.Model):
 
     def __str__(self):
         return self.name
+class PDFFileField(djongo_models.FileField):
+    description = "PDF File"
+
+    def __init__(self, *args, **kwargs):
+        kwargs['format'] = 'pdf'
+        super().__init__(*args, **kwargs)
 class Customerrequirements(models.Model):
     customer = models.ForeignKey(Customerdata, on_delete=models.CASCADE)
-    # phone_no =models.CharField(max_length=50, unique=True)
     meal_preference = models.CharField(max_length=50)
     Part_Name = models.CharField(max_length=50)
     blank_name = models.CharField(max_length=50)
-    draft = models.CharField(max_length=60)
+    upload_file = models.FileField(upload_to='uploads/')
     cname=models.CharField(max_length=50)
     pname=models.CharField(max_length=50)
     cpno=models.CharField(max_length=50)
