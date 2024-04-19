9910bd36a7c182803bfecd58e0d1e7c1bc3c5201
sujallchavan <sujallchavan@gmail.com>
2024-04-19 23:23:15 +0530

    requrment form

--- ErgoAsia_project/customer_app/views.py
+++ ErgoAsia_project/customer_app/views.py
@@ -56,7 +56,7 @@ def customersignin(request):
             # user=authenticate(request,email=email,password=password)
             # if user is not None:
             #     login(request,user)
-            return redirect('Cdashboard')
+            return redirect('customerrequirements')
 
         else:
             msg_invalid="Invalid username and password"
